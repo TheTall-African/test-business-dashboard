bf3d3448bc5393e721f423a094fff6d2404579e0
TheTall-African <arinzechris.ac@gmail.com>
2026-01-20 11:21:49 -0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/ui/global.css';
+import {lusi} from '@/app/ui/fonts';
+import { inter } from '@/app/ui/fonts';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${lusi.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
